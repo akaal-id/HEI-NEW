85536090774f6a81bcbacf0b721fc4b08df28e41
asadomu <asadmuhammad.adrian@gmail.com>
2025-09-02 00:15:33 +0700

    remove helper in exhibitor regist page

--- app/registration/exhibitor/page.tsx
+++ app/registration/exhibitor/page.tsx
@@ -770,9 +770,6 @@ export default function ExhibitorPage() {
         </div>
       </div>
       
-      {/* Google Form Helper */}
-      <GoogleFormHelper />
-      
       {/* Success Modal */}
       {showSuccessModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
